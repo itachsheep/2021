6159cec4ae4e4347a6754ffce4f2559759fe3ac9
itachsheep <itachsheep@163.com>
2021-04-07 23:25:13 +0800

    c语言 指针

--- c_practise/c_jichu_point.c
+++ c_practise/c_jichu_point.c
@@ -13,11 +13,34 @@ int main()
     // printf("a = %d \n",a);
     // printf("*ptr = %d \n", *ptr);
 
-    enum season sea;
-    sea = summer;
 
-    for(sea = spring; sea <= winter; sea++) {
-         printf("sea  =  %d \n",sea);
+    //enum使用
+    // enum season sea;
+    // sea = summer;
+    // for(sea = spring; sea <= winter; sea++) {
+    //      printf("sea  =  %d \n",sea);
+    // }
+
+
+    //指针,内存地址
+    int test = 100;
+    int* p;
+    p = &test;
+    printf("p = %p, *p = %d \n ",p,*p);
+    if(p) {
+        printf("p is not null \n");
+    } 
+    else {
+        printf("p is null \n"); 
+    }
+
+    p = NULL;
+    printf("p = %p \n ",p);
+    if(p) {
+        printf("p is not null \n");
+    } 
+    else {
+        printf("p is null \n");
     }
 
 }
