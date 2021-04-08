58878b52b4cb3b6b0b3b70ad88de69a620456e2d
itachsheep <itachsheep@163.com>
2021-04-08 09:04:54 +0800

    c 语言 函数回调指针

--- c_practise/c_jichu_function_point.c
+++ c_practise/c_jichu_function_point.c
@@ -1,15 +1,42 @@
 #include<stdio.h>
+#include<stdlib.h>
+
 
 int max(int x,int y) 
 {
     return x > y ? x :y;
 }
 
+
+void generate_10_random(int *array, int size, int(*getNextValue)(void))
+{
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = getNextValue();
+    }
+}
+
+int generate_random(void) {
+    return rand();
+}
+
+
 int main()
 {
-    int (* p)(int,int);
-    p = &max;
-    int max = p(100,150);
-    printf("max = %d \n", max);
+    //函数指针
+    // int (* p)(int,int);
+    // p = &max;
+    // int max = p(100,150);
+    // printf("max = %d \n", max);
+
+    //函数指针回调函数
+    int array[10];
+    generate_10_random(array,10,generate_random);
+    for (int i = 0; i < 10; i++)
+    {
+        printf(" %d " , array[i]);
+    }
+    printf("\n");
+
     return 0;
 }
