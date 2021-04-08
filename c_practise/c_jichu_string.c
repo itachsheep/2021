5cce73ddfab808a646c224f4addcf2fffb8fd4bf
itachsheep <itachsheep@163.com>
2021-04-08 22:21:13 +0800

    c语言 字符串

---
+++ c_practise/c_jichu_string.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+
+int main()
+{   
+    char site[6] = {'R', 'U', 'N', 'O', 'O', 'B'};
+    char site2[7] = {'R', 'U', 'N', 'O', 'O', 'B','\0'};
+    
+    printf("site = %s \n",site);
+    printf("site2 = %s \n",site2);
+
+    return 0;
+}
