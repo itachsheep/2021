f9aee7996b6b5607c5e1b46fea0968925fdebc6f
itachsheep <itachsheep@163.com>
2021-04-11 18:13:12 +0800

    c语言 结构体位域

--- c_practise/c_jichu_string.c
+++ c_practise/c_jichu_string.c
@@ -9,28 +9,49 @@ struct Node
 } head = {"c program", 10001};
 
 
+struct bs
+{
+    int a:8;
+    int b:2;
+    int c:6;
+} data;
+
+struct bs2
+{
+    int a;
+    int b;
+    int c;
+}data2;
+
+
+
 int main()
 {   
+    //字符串
     // char site[6] = {'R', 'U', 'N', 'O', 'O', 'B'};
     // char site2[7] = {'R', 'U', 'N', 'O', 'O', 'B','\0'};
     // printf("site = %s \n",site);
     // printf("site2 = %s \n",site2);
 
-    struct Node list[10];
-    struct Node p;
-    struct Node ttNode;
 
-    struct Node *nodePtr = &ttNode;
+    //结构体
+    // struct Node list[10];
+    // struct Node p;
+    // struct Node ttNode;
+    // struct Node *nodePtr = &ttNode;
 
-    strcpy(p.name,"c++ program");
-    p.value = 10002;
+    // strcpy(p.name,"c++ program");
+    // p.value = 10002;
 
-    strcpy(nodePtr->name,"node program");
-    nodePtr->value = 10003;
+    // strcpy(nodePtr->name,"node program");
+    // nodePtr->value = 10003;
 
-    printf("node name: %s, value: %d \n",head.name,head.value);
-    printf("node p: %s, value: %d \n",p.name,p.value);
-    printf("node nodePtr: %s, value: %d \n",nodePtr->name,nodePtr->value);
+    // printf("node name: %s, value: %d \n",head.name,head.value);
+    // printf("node p: %s, value: %d \n",p.name,p.value);
+    // printf("node nodePtr: %s, value: %d \n",nodePtr->name,nodePtr->value);
 
+    //结构体size，结构体位域
+    printf("struct bs size: %lu \n", sizeof(data));
+    printf("struct bs size: %lu \n", sizeof(data2));
     return 0;
 }
