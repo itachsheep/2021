5c9ce8a14b97dd6c5383875ad4950844fc12986d
itachsheep <itachsheep@163.com>
2021-04-14 23:58:06 +0800

    c++ 类，析构函数，this指针，指向类的指针，静态成员

--- c_practise/cpp_jichu_class_construc.cpp
+++ c_practise/cpp_jichu_class_construc.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Line
 {
     public:
+        static int count;
         int width;
         Line();
         Line(int widt);
@@ -12,18 +13,21 @@ class Line
         {
             count = 1;
         }
+        int compare(Line line);
     private:
-         static int count ;
+         
     
 };
 
 Line::Line() 
 {
+    count++;
     cout << "Line constructor: "<< endl;
 }
 
 Line::Line(int wid)
 {
+    count++;
     width = wid;
     cout << "Line constructor: " << width << endl;
 }
@@ -33,11 +37,29 @@ Line::~Line()
     cout << "~Line " << endl;
 }
 
+int Line::compare(Line line)
+{
+    return this->width > line.width;
+}
+
+int Line::count = 0;
+
 int main()
 {
     Line line1;
     Line line2(200);
 
     cout << "---  just do ----" << endl;
+
+    Line line3(300);
+    cout << "line2 - line3: " << line2.compare(line3) << endl;
+    cout << "line3 - line2: " << line3.compare(line2) << endl;
+
+    Line *ptr;
+    ptr = &line2;
+    cout << "ptr: " << ptr->width << endl;
+
+    cout << "count: " << Line::count << endl;
+
     return 0;
 }
