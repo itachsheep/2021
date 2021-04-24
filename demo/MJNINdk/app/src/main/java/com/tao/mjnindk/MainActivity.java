09213b8cff049d51af57643d3d4ce0fa34bd92e2
itachsheep <itachsheep@163.com>
2021-04-24 10:16:10 +0800

    jni ndk ffmpeg ANatvieWIndow渲染视频

--- demo/MJNINdk/app/src/main/java/com/tao/mjnindk/MainActivity.java
+++ demo/MJNINdk/app/src/main/java/com/tao/mjnindk/MainActivity.java
@@ -1,10 +1,19 @@
 package com.tao.mjnindk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     TextView tvVersion;
@@ -12,6 +21,17 @@ public class MainActivity extends AppCompatActivity {
     static {
         System.loadLibrary("life-tip");
     }
+
+
+    private static  final String [] EXAMPLE_LIST = {
+            "FFmpeg + ANativeWindow player",
+            "FFmpeg + OpenGL ES player",
+            "FFmpeg + OpenSL ES visual audio player",
+            "FFmpeg + OpenGL ES VR player",
+            "FFmpeg + single video recorder",
+            "FFmpeg + single audio recorder",
+            "FFmpeg + AV recorder"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +45,84 @@ public class MainActivity extends AppCompatActivity {
 
     public void get_ffm_version(View view) {
         tvVersion.setText(getFFmpegVersion());
+    }
+
+    public void ffm_begin(View view) {
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL,false));
+        MyAdapter myAdapter = new MyAdapter(Arrays.asList(EXAMPLE_LIST));
+        recyclerView.setAdapter(myAdapter);
+        myAdapter.addOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int pos = (int) view.getTag();
+                switch (pos) {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+    }
+
+    class MyViewHolder extends  RecyclerView.ViewHolder {
+        Button tvContent;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvContent = itemView.findViewById(R.id.tvContent);
+        }
+
+        public void setContent(String content) {
+            tvContent.setText(content);
+        }
+
+    }
+
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder> implements View.OnClickListener {
+        List<String> data;
+        View.OnClickListener mOnClickListener;
+
+        public MyAdapter(List<String> datas) {
+            data = datas;
+        }
+
+        public void addOnClickListener(View.OnClickListener onClickListener) {
+            mOnClickListener = onClickListener;
+        }
+
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            MyViewHolder myViewHolder = new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycler,
+                    parent, false));
+            myViewHolder.itemView.setOnClickListener(this);
+            return myViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            holder.setContent(data.get(position));
+            holder.itemView.setTag(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mOnClickListener != null) {
+//                mOnClickListener.onItemClick(v, (Integer) v.getTag());
+                mOnClickListener.onClick(view);
+            }
+        }
     }
 
 
