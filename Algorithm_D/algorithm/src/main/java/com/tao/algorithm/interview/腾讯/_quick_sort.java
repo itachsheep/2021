df2b0ba6e359216ae6f42af1b6d79b394fc506ad
itachsheep <itachsheep@163.com>
2021-07-24 11:18:37 +0800

    快排

---
+++ Algorithm_D/algorithm/src/main/java/com/tao/algorithm/interview/腾讯/_quick_sort.java
@@ -0,0 +1,58 @@
+/**
+ * @ClassName:
+ * @Description:
+ * @author taowei
+ * @version V1.0
+ * @Date
+ */
+
+package com.tao.algorithm.interview.腾讯;
+
+import com.tao.algorithm.utils.Util;
+
+class _quick_sort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{9,8,3,23,45,1,245,76,68,0,5};
+        Util.printArrays(arr);
+        quickSort(arr,0,arr.length - 1);
+        Util.printArrays(arr);
+    }
+
+    
+
+    public static void quickSort(int[] arr,int left,int right) {
+        if(left >= right) {
+            return;
+        }
+
+        int partition = partition(arr,left,right);
+
+        quickSort(arr,left,partition - 1);
+        quickSort(arr,partition + 1,right);
+    }
+
+    public static int partition(int[] arr,int left ,int right) {
+        int pivot = arr[right];
+        int start = left;
+        int end = right;
+        while (start < end) {
+            while (start < end && arr[start] <= pivot)  {
+                start++;
+            }
+            while (start < end && arr[end] >= pivot) {
+                end--;
+            }
+
+            System.out.println("leftPtr: " + start+ ",rightPtr: " + end);
+            swap(arr,start,end);
+        }
+        swap(arr,start,right);
+        return start;
+    }
+
+    public static void swap(int[] arr,int left,int right) {
+        int temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+    }
+}
