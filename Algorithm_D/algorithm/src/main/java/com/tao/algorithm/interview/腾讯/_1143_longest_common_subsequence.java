1abf7549786ed8dcaa61a8aa5d6e17ef0b4a670f
itachsheep <itachsheep@163.com>
2021-07-25 01:50:31 +0800

    两个字符串，最长子串

--- Algorithm_D/algorithm/src/main/java/com/tao/algorithm/interview/腾讯/_1143_longest_common_subsequence.java
+++ Algorithm_D/algorithm/src/main/java/com/tao/algorithm/interview/腾讯/_1143_longest_common_subsequence.java
@@ -11,8 +11,9 @@ package com.tao.algorithm.interview.腾讯;
 class _1143_longest_common_subsequence {
     public static void main(String[] args) {
 
-        String text1 = "abcde", text2 = "ace";
+        String text1 = "abcde", text2 = "abce";
         System.out.println("res = " + longestCommonSubsequence(text1,text2));
+        System.out.println("res = " + longestCommonSubString(text1,text2));
     }
 
     /**
@@ -29,9 +30,9 @@ class _1143_longest_common_subsequence {
 
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i < m ; i++) {
-            char c1 = text1.charAt(i);
+            char c1 = text1.charAt(i-1);
             for (int j = 1; j < n ; j++) {
-                char c2 = text2.charAt(j);
+                char c2 = text2.charAt(j-1);
                 if(c1 == c2) {
                     dp[i][j] = dp[i-1][j-1] + 1;
                     sb.append(c1);
@@ -40,7 +41,31 @@ class _1143_longest_common_subsequence {
                 }
             }
         }
-        System.out.println("sb = " + sb.toString());
-        return dp[m][n];
+        //System.out.println("sb = " + sb.toString());
+        return dp[m-1][n-1];
+    }
+
+    public static String longestCommonSubString(String text1,String text2) {
+        int m = text1.length(), n = text2.length();
+        int max = -1;
+        int[][] dp = new int[m + 1][n + 1];
+        String res = "";
+        for (int i = 1; i < m; i++) {
+            char c1 = text1.charAt(i - 1);
+            for (int j = 1; j < n; j++) {
+                char c2 = text2.charAt(j - 1);
+                if(c1 == c2) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                    if(dp[i][j] > max) {
+                        max = dp[i][j];
+                        res = text1.substring(i-max,i);
+                    }
+                } else {
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+
+            }
+        }
+        return res;
     }
 }
