bdc0047c81c3c5e46a8001bcc83fa8b59a7d5d4a
itachsheep <itachsheep@163.com>
2021-07-30 08:07:07 +0800

    硬币兑换问题

---
+++ Algorithm_D/algorithm/src/main/java/com/tao/algorithm/interview/腾讯/_322_coin_change.java
@@ -0,0 +1,52 @@
+/**
+ * @ClassName:
+ * @Description:
+ * @author taowei
+ * @version V1.0
+ * @Date
+ */
+
+package com.tao.algorithm.interview.腾讯;
+
+import java.util.Arrays;
+
+public class _322_coin_change {
+    /**
+     * 给你一个整数数组 coins ，表示不同面额的硬币；以及一个整数 amount ，表示总金额。
+     *
+     * 计算并返回可以凑成总金额所需的 最少的硬币个数 。如果没有任何一种硬币组合能组成总金额，返回 -1 。
+     *
+     * 你可以认为每种硬币的数量是无限的。
+     *
+     *  
+     *
+     * 示例 1：
+     *
+     * 输入：coins = [1, 2, 5], amount = 11
+     * 输出：3
+     * 解释：11 = 5 + 5 + 1
+     *
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/coin-change
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp,amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if(coins[j] <= i) {
+                    dp[i] = Math.min(dp[i],dp[i - coins[j]] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
