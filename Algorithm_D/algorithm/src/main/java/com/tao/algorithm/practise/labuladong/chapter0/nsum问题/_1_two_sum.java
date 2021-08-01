02f72f8ac482803b10dd1be7dfcdb88c977cae61
itachsheep <itachsheep@163.com>
2021-08-01 16:05:02 +0800

    twoSum问题

--- Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/labuladong/chapter0/nsum问题/_1_two_sum.java
+++ Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/labuladong/chapter0/nsum问题/_1_two_sum.java
@@ -8,6 +8,9 @@
 
 package com.tao.algorithm.practise.labuladong.chapter0.nsum问题;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 class _1_two_sum {
     /**
      * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
@@ -34,13 +37,33 @@ class _1_two_sum {
      * 1，使用hash表
      */
     public int[] twoSum(int[] nums, int target) {
-        
+        HashMap<Integer,Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(hashMap.containsKey(target - nums[i])) {
+                return new int[]{nums[i],target - nums[i]};
+            } else {
+                hashMap.put(nums[i],i);
+            }
+        }
+        return new int[]{-1,-1};
     }
 
     /**
      * 2，使用拉不拉咚方法，先排序，再使用双指针
      */
     public int[] twoSum2(int[] nums, int target) {
-
+        Arrays.sort(nums);
+        int low = 0, high = nums.length - 1;
+        while (low < high) {
+            int sum = nums[low] + nums[high];
+            if(sum < target) {
+                low++;
+            } else if(sum > target) {
+                high--;
+            } else {
+                return new int[]{nums[low], nums[high]};
+            }
+        }
+        return new int[] {-1, -1};
     }
 }
