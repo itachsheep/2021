db47ff0da04515453587645b91012017b29a1055
itachsheep <itachsheep@163.com>
2021-09-07 08:43:07 +0800

    k个一组链表翻转

--- Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/字节/_5_reverse_k_group.java
+++ Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/字节/_5_reverse_k_group.java
@@ -17,9 +17,50 @@ public class _5_reverse_k_group {
         ListNode list = Util.createList(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
         Util.printList(list);
 
-        Util.printList(reversKGroup(list,2));
-        Util.printList(reversKGroup(list,3));
+        //Util.printList(reverseKGroup2(list,2));
+        Util.printList(reverseKGroup2(list,3));
     }
+
+    public static ListNode reverseKGroup2(ListNode head, int k) {
+        ListNode hair = new ListNode(0);
+        hair.next = head;
+        ListNode pre = hair;
+
+        while (head != null) {
+            ListNode tail = pre;
+            // 查看剩余部分长度是否大于等于 k
+            for (int i = 0; i < k; ++i) {
+                tail = tail.next;
+                if (tail == null) {
+                    return hair.next;
+                }
+            }
+            ListNode nex = tail.next;
+            ListNode[] reverse = myReverse2(head, tail);
+            head = reverse[0];
+            tail = reverse[1];
+            // 把子链表重新接回原链表
+            pre.next = head;
+            tail.next = nex;
+            pre = tail;
+            head = tail.next;
+        }
+
+        return hair.next;
+    }
+
+    public static ListNode[] myReverse2(ListNode head, ListNode tail) {
+        ListNode prev = tail.next;
+        ListNode p = head;
+        while (prev != tail) {
+            ListNode nex = p.next;
+            p.next = prev;
+            prev = p;
+            p = nex;
+        }
+        return new ListNode[]{tail, head};
+    }
+
 
     public static ListNode reversKGroup(ListNode head, int k) {
         //在翻转子链表的时候，我们不仅需要子链表头节点 head，
