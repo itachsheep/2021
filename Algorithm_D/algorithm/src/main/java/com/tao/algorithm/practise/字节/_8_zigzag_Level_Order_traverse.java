3de91946b0eb5a75eac9ea2592e28783b7df3abc
itachsheep <itachsheep@163.com>
2021-09-09 20:05:15 +0800

    z型遍历二叉树

--- Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/字节/_8_zigzag_Level_Order_traverse.java
+++ Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/字节/_8_zigzag_Level_Order_traverse.java
@@ -10,7 +10,10 @@ package com.tao.algorithm.practise.字节;
 
 import com.tao.algorithm.utils.TreeNode;
 
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 /**
  * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，
@@ -36,7 +39,48 @@ class _8_zigzag_Level_Order_traverse {
 
     }
 
+    /**
+     * 为了满足题目要求的返回值为「先从左往右，再从右往左」交替输出的锯齿形，
+     * 我们可以利用「双端队列」的数据结构来维护当前层节点值输出的顺序。
+     *
+     *
+     * 双端队列是一个可以在队列任意一端插入元素的队列。
+     * 如果从左至右，我们每次将被遍历到的元素插入至双端队列的末尾。
+     * 如果从右至左，我们每次将被遍历到的元素插入至双端队列的头部。
+     *
+     */
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new LinkedList<List<Integer>>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        nodeQueue.offer(root);
+        boolean isOrderLeft = true;
 
+        while (!nodeQueue.isEmpty()) {
+            Deque<Integer> levelList = new LinkedList<Integer>();
+            int size = nodeQueue.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode curNode = nodeQueue.poll();
+                if (isOrderLeft) {
+                    levelList.offerLast(curNode.val);
+                } else {
+                    levelList.offerFirst(curNode.val);
+                }
+
+                if (curNode.left != null) {
+                    nodeQueue.offer(curNode.left);
+                }
+                if (curNode.right != null) {
+                    nodeQueue.offer(curNode.right);
+                }
+
+            }
+
+            ans.add(new LinkedList<Integer>(levelList));
+            isOrderLeft = !isOrderLeft;
+        }
+        return ans;
     }
 }
