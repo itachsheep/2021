fb1910fcb278af6eda06b47d22450329b8b5ebb1
itachsheep <itachsheep@163.com>
2021-09-09 08:58:44 +0800

    z形遍历二叉树

---
+++ Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/字节/_8_zigzag_Level_Order_traverse.java
@@ -0,0 +1,42 @@
+/**
+ * @ClassName:
+ * @Description:
+ * @author taowei
+ * @version V1.0
+ * @Date
+ */
+
+package com.tao.algorithm.practise.字节;
+
+import com.tao.algorithm.utils.TreeNode;
+
+import java.util.List;
+
+/**
+ * 给定一个二叉树，返回其节点值的锯齿形层序遍历。（即先从左往右，
+ * 再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。
+ *
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ *
+ * 返回锯齿形层序遍历如下：
+ * [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ * ]
+ */
+class _8_zigzag_Level_Order_traverse {
+    public static void main(String[] args) {
+
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+    }
+}
