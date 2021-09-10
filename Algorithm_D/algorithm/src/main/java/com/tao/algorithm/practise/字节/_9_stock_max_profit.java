09c88c84723788a8954cd63fe03ef5ec890a9570
itachsheep <itachsheep@163.com>
2021-09-11 07:32:52 +0800

    买卖股票问题

--- Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/字节/_9_stock_max_profit.java
+++ Algorithm_D/algorithm/src/main/java/com/tao/algorithm/practise/字节/_9_stock_max_profit.java
@@ -30,6 +30,17 @@ class _9_stock_max_profit {
         System.out.println(maxProfit(new int[]{7,5,3,6,4,3,28,34,7,90}));
     }
 
+    /**
+     * 显然，如果我们真的在买卖股票，我们肯定会想：如果我是在历史最低点买的股票就好了！
+     * 太好了，在题目中，我们只要用一个变量记录一个历史最低价格 minprice，
+     * 我们就可以假设自己的股票是在那天买的。那么我们在第 i 天卖出股票能得到的利润就是
+     * prices[i] - minprice。
+     *
+     * 因此，我们只需要遍历价格数组一遍，记录历史最低点，然后在每一天考虑这么一个问题：
+     * 如果我是在历史最低点买进的，那么我今天卖出能赚多少钱？当考虑完所有天数之时，
+     * 我们就得到了最好的答案。
+     *
+     */
     private static int maxProfit(int[] prices) {
         int maxProfit = 0;
         int minPrices = Integer.MAX_VALUE;
