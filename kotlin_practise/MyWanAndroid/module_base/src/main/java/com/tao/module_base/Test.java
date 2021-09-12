31e072bd1ad5c35651db3f5a398f872cfbba071c
itachsheep <itachsheep@163.com>
2021-09-12 22:49:14 +0800

    kotlin 主页，编译依赖问题

---
+++ kotlin_practise/MyWanAndroid/module_base/src/main/java/com/tao/module_base/Test.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName:
+ * @Description:
+ * @author taowei
+ * @version V1.0
+ * @Date
+ */
+
+package com.tao.module_base;
+//import com.hao.library.ui.BaseActivity;
+class Test {
+    void test() {
+
+    }
+}
