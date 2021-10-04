fa3a682c095cbe4b87ada8730256f2950a97fde0
itachsheep <itachsheep@163.com>
2021-10-04 18:06:40 +0800

    kotlin 基本语法

--- kotlin_practise/MyWanAndroid/lib/src/main/java/com/tao/lib/MyClass.kt
+++ kotlin_practise/MyWanAndroid/lib/src/main/java/com/tao/lib/MyClass.kt
@@ -1,8 +1,43 @@
 package com.tao.lib
 
 
+
+
 fun main(args: Array<String>) {
-    println("hello world")
+    defineVariant()
+    stringTemplate()
+}
+
+fun stringTemplate() {
+    var a = 1
+    val s1 = "a is $a"
+    a = 2
+    val s2 = "${s1.replace("is","was")},but now is $a"
+    println(s1)
+    println(s2)
+}
+
+fun defineVariant() {
+    val a: Int = 1
+    val b = 2;
+    val c: Int
+    c = 3
+
+    var x = 5
+    x += 1
+
+    println("a = $a")
+}
+
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun sum2(a: Int, b: Int) = a + b
+
+fun sum3(a: Int, b: Int) {
+    println("sum3 $a + $b = ${a + b}")
 }
 
 class MyClass {
