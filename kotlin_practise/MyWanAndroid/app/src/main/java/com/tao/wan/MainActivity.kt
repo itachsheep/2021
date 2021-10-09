5f43ec86fd8c044e42bfc7a7d4f0a056e457cfb6
itachsheep <itachsheep@163.com>
2021-10-09 14:14:22 +0800

    1，修复viewmodel 和 databinding 生成injector失败问题

--- kotlin_practise/MyWanAndroid/app/src/main/java/com/tao/wan/MainActivity.kt
+++ kotlin_practise/MyWanAndroid/app/src/main/java/com/tao/wan/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.tao.wan
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.hao.library.annotation.AndroidEntryPoint
 import com.hao.library.ui.BaseActivity
 import com.tao.module_wan.viewmodel.UpgradeViewModel
 import com.tao.wan.databinding.AppActivityMainBinding
 
-
+@AndroidEntryPoint
 class MainActivity : BaseActivity<AppActivityMainBinding, UpgradeViewModel>() {
     override fun initData() {
-        TODO("Not yet implemented")
+        viewModel {
+            lifecycle.addObserver(this)
+            
+        }
     }
 
     override fun initView() {
-        TODO("Not yet implemented")
+        viewBinding {
+
+            true
+        }
     }
 }
 
