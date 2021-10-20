535a425477b20bbe63bce78d35cbba86bbd590f0
itachsheep <itachsheep@163.com>
2021-10-20 08:49:30 +0800

    LrnPro 测试协程切换线程

--- kotlin_practise/LrnPro/app/src/main/java/com/tao/lrnpro/MainActivity.kt
+++ kotlin_practise/LrnPro/app/src/main/java/com/tao/lrnpro/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Bundle
 import android.view.View
 import android.widget.TextView
 import com.tao.lrnpro.test.LiveDataTestActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,19 @@ class MainActivity : AppCompatActivity() {
 
     fun goto_live_data_test( view: View) {
         startActivity(Intent(baseContext,LiveDataTestActivity::class.java))
+    }
+
+    fun switch_thread(view: View) {
+        GlobalScope.launch(Dispatchers.IO) {
+            LogUtils.d("start thread id: ${Thread.currentThread().id} }")
+            async {
+               LogUtils.d("async thread id: ${Thread.currentThread().id}")
+            }
+
+            GlobalScope.launch(Dispatchers.Main) {
+                LogUtils.d("switch to main, id: ${Thread.currentThread().id}")
+            }
+        }
     }
 
     /**
