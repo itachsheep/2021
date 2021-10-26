c958b98b7b17371d1720d425ce63ad973086ab83
itachsheep <itachsheep@163.com>
2021-10-26 08:54:17 +0800

    flutter 语法2

--- flutter_practise/flutter_lrn/lib/practise/first_learn.dart
+++ flutter_practise/flutter_lrn/lib/practise/first_learn.dart
@@ -41,8 +41,67 @@ void testString() {
 
   myPrint("validConstString = $validConstString");
   myPrint("invalidConstString = $invalidConstString");
+}
 
+void testList() {
+  var list = [1, 2, 3];
+  myPrint("list 1: ${list[1]}");
+  list.forEach((element) {
+    myPrint("ele: $element");
+  });
+}
+
+void testSet() {
+  var haloSet = {"dd", "ww", "22"};
+  haloSet.add("taowei");
+
+  var nameSet = <String> {};
+  nameSet.add("Jack");
+
+  Set<String> siSet = {};
+  siSet.add("ha");
+
+  var mMap = {};
+
+  const conSet = {
+    "ttt",
+    "ha",
+    "ddd",
+  };
+
+  var mySet = <String>{};
+  mySet.addAll(conSet);
+
+  myPrint("mySet: $mySet");
+}
+
+testMap() {
+  var gitfs = {
+    "key1":"val1",
+    "key2":"val2"
+  };
+  gitfs["key3"] = "val3";
+
+  myPrint("gifts: $gitfs");
+
+  var nMap = {};
+  nMap[1] = "val1";
+  nMap["222"] = "val2";
+  nMap[3] = 3;
+  myPrint("nMap = $nMap");
+}
+
+
+testRune() {
+  var clapping = '\u{1f44f}';
+  myPrint(clapping);
+  myPrint(clapping.codeUnits);
+  myPrint(clapping.runes.toList());
+
+  Runes input = new Runes(
+      '\u2665  \u{1f605}  \u{1f60e}  \u{1f47b}  \u{1f596}  \u{1f44d}');
+  myPrint(new String.fromCharCodes(input));
 }
 void main() {
-  testString();
+  testRune();
 }
