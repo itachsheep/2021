9ab763da847d64feaf2a7ff6b6393becefe315aa
itachsheep <itachsheep@163.com>
2021-12-30 10:00:34 +0800

    测试

--- c_practise/CLionProjects/test/main.cpp
+++ c_practise/CLionProjects/test/main.cpp
@@ -3,5 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World11111!" << std::endl;
+    std::cout << "Hello, World1122!" << std::endl;
     return 0;
 }
