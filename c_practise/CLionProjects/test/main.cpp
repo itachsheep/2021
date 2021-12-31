b0444db051c259b297cb4b64946ac6320b1e7cdc
itachsheep <itachsheep@163.com>
2021-12-31 08:58:56 +0800

    c++ 全局变量 static变量使用

--- c_practise/CLionProjects/test/main.cpp
+++ c_practise/CLionProjects/test/main.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
+using namespace std;
+
+
+extern int globe;
+extern void func();
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World11111!" << std::endl;
     std::cout << "Hello, World1122!" << std::endl;
+
+    cout << "短斤少两发的快递"
+            "拉德斯基发了多少积分"
+            "看极乐世界地方" <<endl;
+    cout << globe <<endl;
+
+    func();
+    func();
+
     return 0;
 }
+
+
